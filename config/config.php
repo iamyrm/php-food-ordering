ba24bdb1c5a9917ba1b5301eaa81e7aac0ffe8a6
Yagyaraj Majhi <majhiyagya123@gmail.com>
2025-06-22 12:46:58 +0545

    config.php done

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+// Avoiding to access congig.php
+if(!isset($_SERVER['HTTP_REFERER'])){
+   header("location: index.php");
+   exit;
+}
+
+try {
+   define("HOST", "localhost");
+   define("DBNAME", "practice-food-ecom");
+   define("USER", "root");
+   define("PASS", "yagya");
+
+   $conn = new PDO("mysql:host=" . HOST . ";dbname=" . DBNAME . ";", USER, PASS);
+   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+   echo $e->getMessage();
+}
